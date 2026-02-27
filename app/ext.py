582ded8fa3b7695279e36afec41e1ae24021a92b
m <mohammed.mm404@gmail.com>
2026-02-27 13:34:42 +0200

    updated flask-login config in ext.py

--- app/ext.py
+++ app/ext.py
@@ -1,3 +1,4 @@
+from flask import redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
@@ -10,8 +11,16 @@ mail = Mail()
 csrf = CSRFProtect()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+login_manager.login_message  = 'Please login to view this page.'
+login_manager.login_message_category = 'info'
 
 def init_auth(user_service):
   @login_manager.user_loader
   def load_user(id):
     return user_service.get(id)
+
+  @login_manager.unauthorized_handler
+  def unauthorized():
+    if request.blueprint == 'api':
+      abort(400)
+    return redirect(url_for('auth.login'))
